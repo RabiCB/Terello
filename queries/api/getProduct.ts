e4fece3086e72a732275694b55eb7d3a2d357a57
Chandra Bhandari <rollinrabin@gmail.com>
2024-04-10 17:23:39 +0545

    detailpage

--- queries/api/getProduct.ts
+++ queries/api/getProduct.ts
@@ -29,6 +29,10 @@ export async function getProductsdetail(slug: any) {
           id
           name
           description
+          price
+         images{
+          url
+         }
         }
        
       }
--- src/app/product/[slug]/page.tsx
+++ src/app/product/[slug]/page.tsx
@@ -1,23 +1,31 @@
-import React from 'react'
-import { getProductsdetail } from '../../../../queries/api/getProduct'
+import React from "react";
+import { getProductsdetail } from "../../../../queries/api/getProduct";
 
-const page = async(props:any) => {
-   const detail=await getProductsdetail(props?.params?.slug)
+const page = async (props: any) => {
+  const detail = await getProductsdetail(props?.params?.slug);
 
-   console.log(detail)
+  console.log(detail);
   return (
     <>
-    {
-      detail?.map((d:any,index:string)=>(
-        <div className="bg-gray-300 rounded-md p-4 flex flex-col items-center cursor-pointer" key={index}>
-          {
-            d?.description
-          }
+      {detail?.map((d: any) => (
+        <div
+          className="grid grid-cols-2 gap-8 max-md:grid-cols-1 max-md:place-content-center max-md:place-items-center"
+          key={d?.id}
+        >
+          <img
+            src={d?.images?.[0]?.url}
+            className="h-full w-full"
+            alt="d;ldld"
+          />
+          <div>
+            <h1 className="text-3xl  font-bold my-1">{d?.name}</h1>
+            <h1 className="text-xl  font-semibold my-1">Rs. {d?.price}</h1>
+            <p className="my-1">{d?.description}</p>
+          </div>
         </div>
-      ))
-    }
+      ))}
     </>
-  )
-}
+  );
+};
 
-export default page
+export default page;
