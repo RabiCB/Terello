ecd15745d0581917a283f56b185ac9a7b1ca9fa7
Chandra Bhandari <rollinrabin@gmail.com>
2024-05-05 21:03:19 +0545

    fix:new layout at /test

--- src/app/test/page.tsx
+++ src/app/test/page.tsx
@@ -1,38 +1,41 @@
+import Imagecarousel from "@/components/Imagecard/Imagecarousel";
+import Link from "next/link";
+import React, { useEffect, useRef } from "react";
+import { getProducts } from "../../../queries/api/getProduct";
+import AddToButton from "@/components/Addtocart/Button";
+import { HomeCarousel } from "@/components/Carousel/HomeCarousel";
 
-import Imagecarousel from '@/components/Imagecard/Imagecarousel'
-import Link from 'next/link'
-import React, { useEffect, useRef } from 'react'
-import { getProducts } from '../../../queries/api/getProduct'
-import AddToButton from '@/components/Addtocart/Button'
-import { HomeCarousel } from '@/components/Carousel/HomeCarousel'
-
-const Page = async() => {
-    const data = await getProducts();
+const Page = async () => {
+  const data = await getProducts();
 
   return (
-    <div className='flex h-[calc(100vh-80px)] gap-4 flex-grow  overflow-y-auto'>
-
-        <div className='   p-2 min-w-[240px] overflow-hidden '>
-            <div className='h-full left overflow-y-auto p-2'>
-            {[...Array(100)].map((_, index) => (
+    <div className="flex h-[calc(100vh-80px)] gap-4 flex-grow  overflow-y-auto">
+      <div className=" max-lg:hidden   p-2 min-w-[240px] overflow-hidden ">
+        <div className="h-full left overflow-y-auto p-2">
+          {[...Array(100)].map((_, index) => (
             <p key={index}>Center Content {index + 1}</p>
           ))}
-            </div>
-
         </div>
-        <div style={{
-            scrollbarWidth:"none"
-        }} className=' flex-1 overflow-y-scroll p-8 mx-auto flex flex-col gap-4 items-center '>
-            <div className='w-full'>
-            <HomeCarousel/>
-            </div>
-         {data?.map((d: any, index: string) => (
+      </div>
+      <div
+        style={{
+          scrollbarWidth: "none",
+        }}
+        className=" flex-1 overflow-y-scroll bg-gray-400 p-8 mx-auto flex flex-col gap-4 items-center "
+      >
+        <div className="w-full">
+          <HomeCarousel />
+        </div>
+        {data?.map((d: any, index: string) => (
           <div
             key={index}
-            className="border-[1px] w-full h-full border-gray-300 rounded-[15px] p-2 flex flex-col items-center cursor-pointer"
+            className="border-[1px] min-w-[60%] max-md:w-[100%]  border-gray-300 rounded-[15px] p-2 flex flex-col items-center cursor-pointer"
           >
-            <Link className="flex flex-col items-center" href={`/product/${d?.slug}`}>
-              <img src={d?.images?.[0]?.url} className="w-full max-h-[320px]"/>
+            <Link
+              className="flex flex-col items-center"
+              href={`/product/${d?.slug}`}
+            >
+              <img src={d?.images?.[0]?.url} className="w-full max-h-[320px]" />
               {/* <Imagecarousel images={d?.images} /> */}
 
               <p>{d?.name}</p>
@@ -41,18 +44,16 @@ const Page = async() => {
             <AddToButton data={d} />
           </div>
         ))}
-            
-        </div>
-        <div className=' w-[240px] p-2 overflow-hidden '>
-        <div  className='h-full left overflow-y-auto  '>
-        {[...Array(100)].map((_, index) => (
+      </div>
+      <div className=" w-[240px] p-2 overflow-hidden max-md:hidden ">
+        <div className="h-full left overflow-y-auto  ">
+          {[...Array(100)].map((_, index) => (
             <p key={index}>Center Content {index + 1}</p>
           ))}
         </div>
-            
-        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Page
+export default Page;
--- src/components/Carousel/HomeCarousel.tsx
+++ src/components/Carousel/HomeCarousel.tsx
@@ -5,7 +5,7 @@ export function HomeCarousel() {
   return (
     <div className="h-56 sm:h-64 xl:h-80 2xl:h-96 my-6 max-md:my-4">
    
-      <Carousel slideInterval={300} slide={true}>
+      <Carousel>
         <img  src="https://www.meme-arsenal.com/memes/0e311f030ab84d928faa0663c1b6a9bd.jpg" alt="..." />
         <img src="https://cdn.shopify.com/s/files/1/1038/1798/products/T-Shirt-Mockup_8a20ae15-6046-4475-ac6a-9e8b5013b42c.jpg?v=1567970252" alt="..." />
         <img src="https://avatars.mds.yandex.net/i?id=1bd7377d59d08a8089fabdbe80ec786f8f1c4840-9233261-images-thumbs&n=13" alt="..." />
